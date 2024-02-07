8baf5671bff4b7995d4c651f49dbb9d3ae0b51f2
Bohdan Solomko <bohdansolomko@gmail.com>
2024-02-07 21:43:54 +0200

    sd

--- tg.js
+++ tg.js
@@ -2,7 +2,7 @@
 async function sendTg(text) {
     let tg = {
         token: "B6869529917:AAHPhFnVk5i2gB-XPiNDoqTb8Mh-S9MiRrw", // Your bot's token that got from @BotFather
-        chat_id: "+tEmTKustFPI3YzA6" // The user's(that you want to send a message) telegram chat id
+        chat_id: "testtgs1" // The user's(that you want to send a message) telegram chat id
     }
 
     const url = `https://api.telegram.org/bot${tg.token}/sendMessage`; // The URL to request
